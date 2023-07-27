2ff20fcc223dbe50ae75d151abc99e8cbf7f13b6
s0j011f <s0j011f@192.168.0.2>
2023-07-27 16:17:25 +0530

    Initial Setup

---
+++ src/main/java/com/anw/managemymoney/ManagemymoneyApplication.java
@@ -0,0 +1,13 @@
+package com.anw.managemymoney;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ManagemymoneyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ManagemymoneyApplication.class, args);
+	}
+
+}
